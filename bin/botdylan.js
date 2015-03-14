50bf3444c0a62b101f0b6b3c996a22c276c078f5
Bob Zoller <bob@zoller.us>
2015-03-14 02:00:49 -0700

    refactor to use liftoff

--- bin/botdylan.js
+++ bin/botdylan.js
@@ -1,28 +1,59 @@
 #!/usr/bin/env node
 
-var program = require('commander')
-  , fs = require('fs')
-  , path = require('path')
-  , config_file
-  , config_options
-  , app_options
-  , default_options = {silent: false, port: 80, auth: 'basic'};
-
+var Liftoff = require('liftoff');
+var argv = require('minimist')(process.argv.slice(2));
+var path = require('path');
+var fs = require('fs');
 GLOBAL._ = require('underscore');
 
-program
-  .version(JSON.parse(fs.readFileSync(__dirname + '/../package.json', 'utf8')).version)
-  .usage('[options]')
-  .option('-d, --dir <path>', 'Configuration and scripts directory', __dirname)
-  .parse(process.argv);
+var BotdylanCli = new Liftoff({
+  name: 'botdylan',
+  extensions: require('interpret').jsVariants
+});
 
-default_options.dir = path.resolve(program.dir);
-config_file = path.resolve(default_options.dir + '/config.json');
+BotdylanCli.launch({
+  cwd: argv.cwd,
+  configPath: argv.botdylanfile,
+  require: argv.require,
+  completion: argv.completion,
+  verbose: argv.verbose
+}, function(env) {
 
-console.log('* Scripts path: ' + default_options.dir);
-console.log('* Configuration path: ' + config_file);
+  argv.dir = argv.dir || env.configBase;
 
-config_options = require('cjson').load(config_file);
-app_options = _.extend(default_options, config_options);
+  var command = argv._[0];
 
-require('../lib/bootstrap')(app_options);
+  if (argv.v || argv.version) {
+    console.log(JSON.parse(fs.readFileSync(__dirname + '/../package.json', 'utf8')).version);
+    process.exit(0);
+  }
+
+  if (argv.help) {
+    console.log('usage: botdylan [--dir <config directory>]');
+    process.exit(0);
+  }
+
+  if (!env.configPath) {
+    if (command === 'init') {
+      fs.writeFileSync(path.join(process.cwd(), 'Botdylanfile.js'), fs.readFileSync(path.resolve(__dirname, '../Botdylanfile.js.sample'), 'utf8'), 'utf8');
+      console.log("wrote Botdylanfile.js");
+      process.exit(0);
+    } else {
+      console.error('No Botdylanfile found. Use `botdylan init` to generate one.');
+      process.exit(1);
+    }
+  }
+
+  process.chdir(env.configBase);
+
+  var config = {};
+  config = _.extend(config, require(env.configPath));
+  if (argv.dir)
+    config.dir = argv.dir;
+
+  console.log('* Scripts path: ' + config.dir);
+  console.log('* Configuration path: ' + env.configPath);
+  
+  require('../lib/bootstrap')(config);
+});
+
